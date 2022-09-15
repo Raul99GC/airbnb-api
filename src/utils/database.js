dbf991616a7019399ec8ed84fabb924777122a17
Raul C <raul.gines1999@gmail.com>
2022-09-15 13:24:45 -0500

    agregando example.config.env y modificiando database.js para probar la coneccion al server

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,13 +1,22 @@
 const {Sequelize} = require('sequelize')
 
-const db = new Sequelize({
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'root',
-    database: 'airbnb',
-    port: 5432
-})
+const db = new Sequelize({ 
+    dialect: 'postgres', 
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    logging: false,
+    dialectOptions:
+      process.env.NODE_ENV === 'production'
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+  });
 
 module.exports = {
     db
